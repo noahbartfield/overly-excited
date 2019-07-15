eec6b7f7b9a2d2139affcd7b4fe5653fb2942035
Noah Bartfield <noahbartfield@Noahs-MacBook-Pro.local>
2019-07-15 13:32:05 -0500

    Finished multiple punctuation on excercise

--- scripts/excited.js
+++ scripts/excited.js
@@ -1,6 +1,26 @@
-const addExcitement = function (message) {
-    console.log(`The message you have for the console is: "${message}". Is this correct?`)
-    return(addExcitement)
+// const addExcitement = function (message) {
+//     console.log(`The message you have for the console is: "${message}". Is this correct?`)
+//     return(addExcitement)
+// }
+
+// addExcitement("I've created a working function")
+
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+
+function addExcitement(theWordArray, punctuation, quantity) {
+
+    let buildMeUp = ""
+
+    for (let i = 0; i < sentence.length; i++) {
+        const wordBuilder = sentence[i]
+
+        if ((i + 1) % 3 === 0) {
+            buildMeUp += `${wordBuilder}${punctuation.repeat(quantity)} `
+        } else {
+            buildMeUp += `${wordBuilder} `
+        }
+        console.log(buildMeUp)
+    }
 }
 
-addExcitement("I've created a working function")
+addExcitement(sentence, "*", 4)
