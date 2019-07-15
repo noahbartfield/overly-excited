7ba520d9ea26fd1c23a4e961d25b4ce3eb754042
Noah Bartfield <noahbartfield@Noahs-MacBook-Pro.local>
2019-07-15 10:59:06 -0500

    Completely overly excited function excercise

---
+++ scripts/excited.js
@@ -0,0 +1,6 @@
+const addExcitement = function (message) {
+    console.log(`The message you have for the console is: "${message}". Is this correct?`)
+    return(addExcitement)
+}
+
+addExcitement("I've created a working function")
